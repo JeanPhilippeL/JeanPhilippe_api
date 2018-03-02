d5f574c9cce8c6010d118c5f738b59104ac6a19b
JP_Leclerc <jp_leclerc01@hotmail.fr>
2018-03-02 15:27:56 -0500

    Receive user token

--- routes/api.php
+++ routes/api.php
@@ -18,5 +18,21 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 Route::get('users/{user}/profile', 'ProfileController@show');
 Route::put('users/{user}/profile', 'ProfileController@update');
+Route::get('/create-personal-token', function () {
+    $rnd = random_int(0, 1000);
+    $user = new App\User();
+    $user->name = $rnd.'oauth';
+    $user->password = Hash::make('secret');
+    $user->email = $rnd.'oauth@mail.com';
+    $user->save();
+    $token = $user->createToken('iot')->accessToken;
+    echo $token;
+});
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::middleware('auth:api')->get('/user/profile', function (Request $request) {
+    return $request->user()->profile;
+});
 
 
