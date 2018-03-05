4a5f5de4c14801af764930ae1c67bcf6fa5bef97
JP_Leclerc <jp_leclerc01@hotmail.fr>
2018-03-04 22:02:35 -0500

    Added test class

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Passport\Passport;
+
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testServerCreation()
+    {
+        Passport::actingAs(
+            \App\User::find(1)
+        );
+        $response = $this->post('/register',
+            ['name'=> 'Alia',
+            'email'=> 'alia@hotmail.com',
+            'password'=> '123456'],
+            ['Accept'=> 'application/json']);
+
+        echo (json_decode($response));
+        $response->assertStatus(200);
+    }
+
+}
