8f4e0f8cc599c42dfd29780081e7173ed0b86f79
JP_Leclerc <jp_leclerc01@hotmail.fr>
2018-03-06 10:38:19 -0500

    Working User

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use function MongoDB\BSON\fromJSON;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -21,20 +22,112 @@ class UserTest extends TestCase
         $this->assertTrue(true);
     }
 
-    public function testPostUser()
+    public function testPostUserStatus()
     {
 
         $response = $this->post('api/register',
             ['name'=> 'Alia',
-            'email'=> 'alia@hotmail.com',
-            'password'=> '123456'],
+            'email'=> 'aliamail@hotmail.com',
+            'password'=> 'abc123456'],
             ['Accept'=> 'application/json']);
 
         /*Passport::actingAs(
             \App\User::find(1)
         );*/
-        //echo (json_decode($response));
-        $response->assertStatus(201)->assertStatus(['accesstoken']);
+
+        $response->assertStatus(201)->assertJsonStructure(['accesstoken']);
+    }
+
+    public function testPostUserNameTooLong()
+    {
+
+        $response = $this->post('api/register',
+            ['name'=> 'AliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaaAliaa',
+                'email'=> 'aliamailasd@hotmail.com',
+                'password'=> 'abc123456'],
+            ['Accept'=> 'application/json']);
+
+        $response->assertStatus(422);
+    }
+
+    public function testPostUserNameNotString()
+    {
+
+        $response = $this->post('api/register',
+            ['name'=> 30,
+                'email'=> 'aliamailasd@hotmail.com',
+                'password'=> 'abc123456'],
+            ['Accept'=> 'application/json']);
+
+        $response->assertStatus(422);
+    }
+
+    public function testPostUserNoName()
+    {
+
+        $response = $this->post('api/register',
+            [   'email'=> 'aliamailasd@hotmail.com',
+                'password'=> 'abc123456'],
+            ['Accept'=> 'application/json']);
+
+        $response->assertStatus(422);
+    }
+
+    public function testPostUserNotEmail()
+    {
+
+        $response = $this->post('api/register',
+            ['name'=> 'Mathieu',
+                'email'=> 'MathMailBox',
+                'password'=> 'abc123456'],
+            ['Accept'=> 'application/json']);
+
+        $response->assertStatus(422);
+    }
+
+    public function testPostUserNameNoEmail()
+    {
+
+        $response = $this->post('api/register',
+            ['name'=> 30, 'password'=> 'abc123456'],
+            ['Accept'=> 'application/json']);
+
+        $response->assertStatus(422);
+    }
+
+    public function testPostUserNotUniqueEmail()
+    {
+
+        $response = $this->post('api/register',
+            ['name'=> 'Alia',
+                'email'=> 'aliamail@hotmail.com',
+                'password'=> 'abc123456'],
+            ['Accept'=> 'application/json']);
+
+        $response->assertStatus(422);
+    }
+
+    public function testPostUserEmailTooLong()
+    {
+
+        $response = $this->post('api/register',
+            ['name'=> 'Mathieu',
+                'email'=> 'abcdeiamailasdaliamailasdaliamailasdaliamailasdaliamailasdaliamailasdaliamailasdaliamailasdaliamailasdaliamailasdaliamailasdaliamailasdaliamailasdaliamailasdaliamailasdaliamailasdaliamailasdaliamailasdaliamailasdaliamailasdaliamailasdaliamailasdaliamailasd@hotmail.com',
+                'password'=> 'abc123456'],
+            ['Accept'=> 'application/json']);
+
+        $response->assertStatus(422);
+    }
+
+    public function testPostUserNoPasword()
+    {
+
+        $response = $this->post('api/register',
+            ['name'=> 'Mathieu',
+                'email'=> 'aliamail123@hotmail.com'],
+            ['Accept'=> 'application/json']);
+
+        $response->assertStatus(422);
     }
 
 }
